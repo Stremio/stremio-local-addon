d8d724b83ecb3953634abd2daf0c2bcd7f9ab0cc
Ivo Georgiev <ivo@strem.io>
2018-06-01 08:19:19 +0300

    add storage test

--- test/index.js
+++ test/index.js
@@ -1,1 +1,2 @@
+require('./storage')
 require('./addon')
---
+++ test/storage.js
@@ -0,0 +1,59 @@
+const os = require('os')
+const crypto = require('crypto')
+const path = require('path')
+
+const Storage = require('../lib/storage')
+
+const tmpPath = path.join(os.tmpdir(), 'storage'+crypto.randomBytes(4).readUInt32LE(0))
+
+const tape = require('tape')
+
+let storage1
+let storage2
+
+tape('storage: can construct', function(t) {
+	storage1 = new Storage()
+	t.ok(storage1, 'object returned')
+	t.ok(storage1.byFilePath, 'byFilePath exists')
+	t.end()
+})
+
+tape('storage: can load an empty storage', function(t) {
+	storage1.load(tmpPath, function(err) {
+		t.error(err)
+		t.end()
+	})
+})
+
+
+function checkAllData(t, storage) {
+	t.equals(storage.byFilePath.size, 2)
+	t.equals(storage.byFilePath.get('/file/test1').itemId, 'test1')
+	t.equals(storage.byFilePath.get('/file/test2').itemId, 'test2')
+	t.deepEqual(storage.byItemId.get('test1').get('/file/test1'),
+		 { itemId: 'test1', files: [{ path: '/file/test1' }] }
+	)
+}
+
+tape('storage: can persist', function(t) {
+	storage1.saveEntry('/file/test1', { itemId: 'test1', files: [{ path: '/file/test1' }] }, function(err) {
+		t.error(err)
+
+		storage1.saveEntry('/file/test2', { itemId: 'test2', files: [{ path: '/file/test2', name: 'test\nt' }] }, function(err) {
+			t.error(err)
+			checkAllData(t, storage1)
+			t.end()
+		})
+	})
+})
+
+
+
+tape('storage: can load', function(t) {
+	storage2 = new Storage()
+	storage2.load(tmpPath, function(err) {
+		t.error(err)
+		checkAllData(t, storage2)
+		t.end()
+	})
+})
